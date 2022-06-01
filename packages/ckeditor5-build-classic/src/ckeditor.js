67b140240c2deb7cef904b8d5672757143756689
Wes Cossick <wes@sparksuite.com>
2022-06-01 15:14:44 -0500

    Final tweaks

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -214,11 +214,6 @@ MarkdownEditor.defaultConfig = {
 			'redo'
 		]
 	},
-	image: {
-		toolbar: [
-			'linkImage'
-		],
-	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'en'
 };
